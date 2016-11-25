fc6c7888f250b6cc0322fa18418c59dd458fcec2
Mark Nicola <mark.is.at.a.computer@gmail.com>
2016-11-25 13:15:26 -0800

    mobile section browse by swipe

--- js/app.js
+++ js/app.js
@@ -3,33 +3,84 @@ var opts = {
   autoplay: false,
   slides: []
 },
+getSection = function () {
+  return parseInt($("body").attr("data-section"));
+},
+setSection = function (section) {
+  if (testSection(section)) {
+    $("body").attr("data-section", section);
+  }
+},
 loadSection = function (section) {
-  //  get section data
-  $.getJSON('/data/'+section+'.json', function(sectionSlides){
-    //  random key from slides
-    var rando = Math.round((sectionSlides.length-1)*Math.random());
-    //  set slides
-    opts.slides = sectionSlides;
-    //  put rando in first to be bg
-    if (typeof opts.slides[rando] == "object" && rando > 0) {
-      var slide = opts.slides[rando];
-      opts.slides.splice(rando,1);
-      opts.slides.unshift(slide);
+  //  check data exists
+  $.ajax({
+    url: '/data/' + section + '.json',
+    type: 'HEAD'
+  })
+  .done(function(data, status,xhr) {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      //  get section data
+      $.getJSON('/data/'+section+'.json', function(sectionSlides){
+      //  random key from slides
+        var rando = Math.round((sectionSlides.length-1)*Math.random());
+        //  set slides
+        opts.slides = sectionSlides;
+        //  put rando in first to be bg
+        if (typeof opts.slides[rando] == "object" && rando > 0) {
+          var slide = opts.slides[rando];
+          opts.slides.splice(rando,1);
+          opts.slides.unshift(slide);
+        }
+        //  attach vegas to body
+        if (typeof $("body")[0]._vegas == "object") {
+          //  update options with new slide set
+          $("body").vegas("options", opts);
+          $("body").vegas("jump", 1);
+          //  remove old slide
+          var slides = $(".vegas-slide");
+          if (slides.length > 1) {
+            $(".vegas-slide").first().remove();
+          }
+        } else {
+          $("body").vegas(opts);
+        }
+        //  store section
+        $("body").attr("data-section", section);
+      });
     }
-    //  attach vegas to body
-    $("body").vegas(opts);
   });
-}
+},
+changeSection = function(direction) {
+  var section = $("body").attr('data-section');
+  switch(direction) {
+    case "up":
+      section++;
+      break;
+    case "down":
+      section--;
+      break;
+  }
+  loadSection(section);
+};
 
 $(document).ready(function($) {
-  var section = 0;
   //  init with first section
   loadSection(0);
-  //  mobile events
+  //  mobile
   if ($.os.phone) {
-    $("body").on("click", function (e) {
+      //  mobile events
+    $("body").on("singleTap", function (e) {
       $("body").vegas("next");
-    })
+    });
+    $("body").on("doubleTap", function (e) {
+      $("a.gumroad-button").trigger("click");
+    });
+    $("body").on("swipeRight", function (e) {
+      changeSection("down");
+    });
+    $("body").on("swipeLeft", function (e) {
+      changeSection("up");
+    });
   //  browser events
   } else {
     //  mouseover events
